e1cf339a853485e2a784e714beaca7ef9f87eab9
Abdullah Ehsan <45837088+AbdullahEhsan@users.noreply.github.com>
2020-09-26 14:26:17 -0400

    Add files via upload

---
+++ CountingValleys.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println(countingValleys(8, "UDDDUDUU"));
+  }
+
+public static int countingValleys(int steps, String path) {
+        int valleys = 0;
+        int currentLevel = 0;
+        for(int i = 0; i<path.length();i++){
+            if(path.charAt(i)==('D')){
+                currentLevel--;
+            }
+            if(path.charAt(i)==('U')){
+                currentLevel++;
+                if(currentLevel==0) valleys++;
+            }
+        }
+        return valleys;
+    }
+}
