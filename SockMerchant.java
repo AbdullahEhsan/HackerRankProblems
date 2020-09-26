24d812b7edf788556264f3aa4eccea8ad262a910
Abdullah Ehsan <45837088+AbdullahEhsan@users.noreply.github.com>
2020-09-26 14:23:41 -0400

    Add files via upload

---
+++ SockMerchant.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Main {
+  public static void main(String[] args) {
+    int[] socks = {1,1,3,1,2,1,3,3,3,3};
+    System.out.println(sockMerchant(10, socks));
+  }
+
+  static int sockMerchant(int n, int[] ar) {
+        Arrays.sort(ar);
+        int matches = 0;
+        for(int i = 0; i<ar.length-1; i++){
+        if(ar[i]==ar[i+1]){
+            matches++;
+            i++;
+        }
+    }
+    return matches;
+  }
+}
